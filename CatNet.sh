2846bd1e46137a55ca630db320793d0226b69206
scavMeet <jhrgreferde@tutamail.com>
2026-06-20 15:05:03 +0300

    Initial commit: CatNet port manager

---
+++ CatNet.sh
@@ -0,0 +1,33 @@
+echo 'Created by https://github.com/scavMeet'
+
+
+echo "hi $USER"
+while true; do
+    echo "------ ACTIVE TCP PORTS ------"
+    sudo ss -tulpn | awk -F'[: ]+' '/tcp/ {print $6}'
+    echo "commands: open <port>, close <port>"
+    read -p "input command: " action port
+    if [[ -n "$port" && ! "$port" =~ ^[0-9]+$ ]]; then
+        echo "Ошибка: $port не является числом!"
+        sleep 2
+        continue
+    fi
+    case $action in
+        open)
+            echo "Open port $port...."
+            nc -l -p "$port" > /dev/null 2>&1 &
+            ;;
+        close)
+            echo "Close port $port..."
+            sudo fuser -k "$port/tcp"
+            ;;
+        exit)
+            break
+            ;;
+        *)
+            echo "uncorrect command, enter: open <port >or close <port>"
+            ;;
+    esac
+    sleep 1
+done
+
